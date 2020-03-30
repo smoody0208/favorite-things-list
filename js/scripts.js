8b757cc790d98fdf1cfda94ab6320355fabe46c2
Spencer Moody <spencer.moody@outlook.com>
2020-03-30 10:11:01 -0700

    add the first variables to receive input
    coauthored by: Taylor Phillips <taylorphillips133@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+
+  var animalInput = $("input#animal").val();
+
+  var sportInput = $("input#sport").val();
+
+  
+  
+  }
+}
