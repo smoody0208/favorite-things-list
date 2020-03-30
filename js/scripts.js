0c8f66a08174ef39bb925011a56c748fed237d10
Spencer Moody <spencer.moody@outlook.com>
2020-03-30 10:28:07 -0700

    create and formulate the array with our variables inside
    coauthored by: Taylor Phillips <taylorphillips133@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-
   var animalInput = $("input#animal").val();
-
   var sportInput = $("input#sport").val();
-
   var colorInput = $("input#color").val();
-
   var seasonInput = $("input#season").val();
-  
-  }
-}
+
+  var array = [animalInput, sportInput, colorInput, seasonInput]
+  $(".animal").text(array[0]);
+  $(".sport").text(array[1]);
+  $(".color").text(array[2]);
+  $(".season").text(array[3]);
+  });
+});
